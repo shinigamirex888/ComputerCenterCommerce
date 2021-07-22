d928d25fc3445d7012463d01540b8bf5f10eac4b
Snehil <snehilsneh888@gmail.com>
2021-07-22 12:56:23 +0530

    Added errorMiddleware for custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-import express from'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+
+
+
 
 
 dotenv.config();
@@ -11,12 +15,12 @@ connectDB();
 
 const app = express();
 
+app.use("/api/products", productRoutes);
 
-app.use('/api/products', productRoutes);
+app.use(notFound);
 
+app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT=process.env.PORT || 5000;
-
-app.listen(PORT,console.log('server started!!'.yellow.bold));
+app.listen(PORT, console.log("server started!!".yellow.bold));
