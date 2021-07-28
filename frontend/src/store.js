feb929fe11cbef58686fbc32939e8be4276b3679
Snehil <snehilsneh888@gmail.com>
2021-07-28 16:50:50 +0530

    Added cartReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,14 @@ import { createStore,combineReducers,applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import { productListReducer,productDetailsReducer } from "./reducers/productReducers";
+import {cartReducer} from "./reducers/cartReducer"
+
+
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 })
 
 const initialState = {};
