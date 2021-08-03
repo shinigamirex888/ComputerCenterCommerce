1e05981818e6f61ddef4eb430c332dab86395a08
Snehil <snehilsneh888@gmail.com>
2021-08-03 20:57:35 +0530

    Logout Action

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,19 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import { Container, Navbar, Nav,NavDropdown } from "react-bootstrap";
+import {logout} from "../actions/userActions"
+
 
 const Header = () => {
 
   const dispatch = useDispatch();
-  
+
 
   const userLogin=useSelector(state=>state.userLogin)
   const {userInfo}=userLogin
 
   const logoutHandler=()=>{
-    console.log('logout')
+    dispatch(logout())
   }
 
   return (
