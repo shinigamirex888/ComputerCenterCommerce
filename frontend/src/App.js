bf6440e43e29e81eab7846c3f1c68ab2a2361d40
Snehil <snehilsneh888@gmail.com>
2021-08-04 17:09:23 +0530

    Added Register Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
 
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
     <main className='py-3'>
       <Container>
         <Route path='/login' component={LoginScreen}  />
+        <Route path='/register' component={RegisterScreen}  />
         <Route path='/product/:id' component={ProductScreen}  />
         <Route path='/cart/:id?' component={CartScreen}  />
         <Route path='/' component={HomeScreen} exact />
