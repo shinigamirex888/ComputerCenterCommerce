468f7173cce6e0110161216bc49501327f885967
Snehil <snehilsneh888@gmail.com>
2021-08-05 10:12:07 +0530

    Added backend controller for user update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,6 +51,39 @@ const getUserProfile=asyncHandler(async(req,res)=>{
 })
 
 
+//update user profile
+//user/profile
+//private,PUT
+const updateUserProfile=asyncHandler(async(req,res)=>{
+    const user=await User.findById(req.user._id)
+
+    if(user){
+        user.name=req.body.name ||user.name
+        user.email=req.body.email ||user.email
+        if(req.body.password){
+            user.password=req.body.password
+        }
+        const updatedUser=await user.save()
+
+        res.json({
+            _id:updatedUser._id,
+            name:updatedUser.name,
+            email:updatedUser.email,
+            isAdmin:updatedUser.isAdmin,
+            token:generateToken(updatedUser._id)
+        })
+
+    }else{
+        res.status(404)
+        throw new Error('User not found');
+    }
+   
+})
+
+
+
+
+
 
 // POST/Register a new user
 //api/users
@@ -86,4 +119,4 @@ const registerUser=asyncHandler(async(req,res)=>{
 })
 
 
-export{authUser,registerUser,getUserProfile}
+export{authUser,registerUser,getUserProfile,updateUserProfile}
