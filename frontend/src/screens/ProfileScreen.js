cadea26f11250fddb53bd7cbaadd86c722cf5a8b
Snehil <snehilsneh888@gmail.com>
2021-08-06 17:00:20 +0530

    Update user profile Action and functionality on profile Screen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { Form, Row, Col, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails,updateUserProfile } from "../actions/userActions";
 
 const ProfileScreen = ({location,history}) => {
   const [name, setName] = useState("");
@@ -21,6 +21,10 @@ const ProfileScreen = ({location,history}) => {
   
   const userLogin = useSelector(state => state.userLogin);
   const {userInfo}=userLogin;
+
+  const userUpdateProfile = useSelector(state => state.userUpdateProfile);
+  const {success}=userUpdateProfile;
+
 
 
   useEffect(() => {
@@ -45,7 +49,7 @@ const ProfileScreen = ({location,history}) => {
       }
       else{
       //Dispatch update
-      
+      dispatch(updateUserProfile({id:user._id,name,email,password }))
       }
   }
 
@@ -54,6 +58,7 @@ const ProfileScreen = ({location,history}) => {
      <h2>User Profile</h2>
       {message && <Message variant='danger'>{message}</Message>}
       {error && <Message variant='danger'>{error}</Message>}
+      {success && <Message variant='success'>Profile Updated</Message>}
       {loading && <Loader/>}
       <Form onSubmit={submitHandler}>
 
