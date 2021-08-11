a2cb3549fd3402e9d60e0784fa10836fde9db26e
Snehil <snehilsneh888@gmail.com>
2021-08-11 20:25:00 +0530

    Get order by id backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -13,28 +13,41 @@ const addOrderItems = asyncHandler(async (req, res) => {
     totalPrice,
   } = req.body;
 
-  if(orderItems && orderItems.length === 0) {
-      res.status(400)
-      throw new Error('No order items')
-      return
-  }else{
-      const order=new Order({
-        orderItems,
-        user: req.user._id,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,                      
-      })
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
 
-      const createdOrder=await order.save()
-      
-      res.status(201).json(createdOrder)  
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
   }
 });
 
+// GET /Get order by ID /api/orders/:id
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
 
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
 
-export{addOrderItems}
+export { addOrderItems,getOrderById };
