e689b71e0db318e2afd641a9cffee4b6dbc6e06a
Snehil <snehilsneh888@gmail.com>
2021-08-13 18:23:55 +0530

    Added paid and delivered status on order summary

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,49 +6,56 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { getOrderDetails } from "../actions/orderActions";
 
+const OrderScreen = ({ match }) => {
+  const orderId = match.params.id;
 
+  const dispatch = useDispatch();
 
-const OrderScreen = ({match}) => {
-    const orderId=match.params.id
- 
- 
-    const dispatch = useDispatch();
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order, loading, error } = orderDetails;
 
-    
-
-  
-  const orderDetails=useSelector(state => state.orderDetails)
-  const {order,loading,error}=orderDetails
-
-
-  if(!loading){
-  order.itemsPrice = order.orderItems.reduce(
-    (acc, item) => acc + item.price * item.qty,
-    0
-  );
+  if (!loading) {
+    order.itemsPrice = order.orderItems.reduce(
+      (acc, item) => acc + item.price * item.qty,
+      0
+    );
   }
 
-  useEffect(()=>{
-    dispatch(getOrderDetails(orderId))
-  },[])
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId));
+  }, []);
 
- 
-
-  return loading ? <Loader/> : error ? <Message variant="danger">{error}</Message> :
-  <>
-    <h1>Order {order._id}</h1>
-    <Row>
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
         <Col md={8}>
           <ListGroup variant="flush">
             <ListGroup.Item>
               <h2>Shipping</h2>
-              <p><strong>Name:</strong>{order.user.name}</p>
-              <p><strong>Email:</strong> <a href={`mailto:${order.user.email}`}>{order.user.email}</a></p>
+              <p>
+                <strong>Name:</strong>
+                {order.user.name}
+              </p>
+              <p>
+                <strong>Email:</strong>{" "}
+                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+              </p>
               <p>
                 <strong>Address:</strong>
                 {order.shippingAddress.address},{order.shippingAddress.city},
-                {order.shippingAddress.postalCode},{order.shippingAddress.country}
+                {order.shippingAddress.postalCode},
+                {order.shippingAddress.country}
               </p>
+              {order.isDelivered ? (
+                <Message variant="success">Delivered on {order.deliveredAt}</Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
@@ -57,6 +64,11 @@ const OrderScreen = ({match}) => {
                 <strong>Method:</strong>
                 {order.paymentMethod}
               </p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not paid</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
@@ -126,16 +138,12 @@ const OrderScreen = ({match}) => {
                   <Col>₹{order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-
-              
-
-
-              
             </ListGroup>
           </Card>
         </Col>
       </Row>
-  </> 
+    </>
+  );
 };
 
 export default OrderScreen;
