2bd13ca379809ddf09f2fe92a56e9db5957ef18c
Snehil <snehilsneh888@gmail.com>
2021-08-23 23:36:24 +0530

    Adding UserEditScreen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -23,7 +23,6 @@ router
   .route("/:id")
   .delete(protect, admin, deleteUser)
   .get(protect, admin, getUserById)
-  .put(protect, admin, getUserById)
-  .put(protect / admin / updateUser);
+  .put(protect , admin , updateUser);
 
 export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,9 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+
+
 
 
 
@@ -37,6 +40,7 @@ function App() {
         <Route path='/product/:id' component={ProductScreen}  />
         <Route path='/cart/:id?' component={CartScreen}  />
         <Route path='/admin/userlist' component={UserListScreen}  />
+        <Route path='/admin/user/:id/edit' component={UserEditScreen}  />
         <Route path='/' component={HomeScreen} exact />
      </Container>
     </main>
