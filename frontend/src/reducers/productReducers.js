5f3f97cd9fc39d04ed6182d9c7f35427f98ff740
Snehil <snehilsneh888@gmail.com>
2021-08-26 00:02:33 +0530

    Added Admin delete product backend and frontend

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,6 +5,9 @@ import{
      PRODUCT_LIST_REQUEST,
      PRODUCT_LIST_SUCCESS,
      PRODUCT_LIST_FAIL, 
+     PRODUCT_DELETE_REQUEST,
+     PRODUCT_DELETE_SUCCESS,
+     PRODUCT_DELETE_FAIL,
      } from "../constants/productConstants"
 ;
 
@@ -41,3 +44,17 @@ export const productDetailsReducer=(state={product:{reviews:[]}},action)=>{
 } 
 
 
+
+
+export const productDeleteReducer=(state={},action)=>{
+    switch(action.type){
+        case PRODUCT_DELETE_REQUEST:
+            return {loading:true}
+        case PRODUCT_DELETE_SUCCESS:
+            return {loading:false,success:true}
+        case PRODUCT_DELETE_FAIL:
+            return {loading:false,error:action.payload}
+        default:
+            return state    
+    }
+}
