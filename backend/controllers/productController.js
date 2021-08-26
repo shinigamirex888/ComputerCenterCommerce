709ba4d6dd1727223651b974ff6ae19f2558cf5e
Snehil <snehilsneh888@gmail.com>
2021-08-27 00:12:59 +0530

    Backend for create and update product by admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -41,8 +41,75 @@ const deleteProduct=asyncHandler(async(req,res)=>{
 })
 
 
+
+
+
+// Createproduct/POST /api/products/private,admin
+const createProduct=asyncHandler(async(req,res)=>{
+    const product=new Product({
+        name:'Sample name',
+        price:0,
+        user:req.user._id,
+        image:'/images/sample.jpg',
+        brand:'Sample brand',
+        category:'Sample category',
+        countInStock:0,
+        description:'Sample description',
+        numReviews:0,
+    })
+
+    const createdProduct=await product.save();
+    res.status(201).json(createdProduct);
+})
+
+
+
+
+
+// Updateproduct/PUT /api/products/:id/private,admin
+const updateProduct=asyncHandler(async(req,res)=>{
+    const {
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description,
+        numReviews,
+    }=req.body;
+
+    const product=await Product.findById(req.params.id);
+
+    if(product){
+        product.name=name;
+        product.price=price;
+        product.image=image;
+        product.brand=brand;
+        product.category=category;
+        product.countInStock=countInStock;
+        product.description=description;
+        product.numReviews=numReviews;
+
+        const updatedProduct=await product.save();
+        res.json(updatedProduct);
+    }else{
+        res.status(404)
+        throw new Error(" Product Not Found");
+    }
+
+
+    
+})
+
+
+
+
+
 export{
     getProducts,
     getProductById,
     deleteProduct,
+    updateProduct,
+    createProduct,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,23 @@
-import express from 'express';
-const router=express.Router();
-import {getProducts,getProductById,deleteProduct} from"../controllers/productController.js"
+import express from "express";
+const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  createProduct,
+} from "../controllers/productController.js";
 
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 // GET /products
-router.route('/').get(getProducts);
-  
-  // GET /products/:id
-  router.route('/:id').get(getProductById).delete(protect,admin,deleteProduct)
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
-
-
+// GET /products/:id
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
